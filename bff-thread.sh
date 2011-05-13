c000111beb34ba4ea5b28ee3646ee552834e4064
Thad Ward <coderjoe69@yahoo.com>
2011-05-13 19:22:28 -0400

    use bash math for better compatibility
    modified from pull request submitted by ip2k

--- bff-thread.sh
+++ bff-thread.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #./bff-thread.sh start end
-seq $1 $2 | while read id; do
+for ((id=${1}; id<=${2}; id++)); do
   ./bff.sh $id cookies-\$\$.txt;
   if [ -f STOP ]; then
     echo "Stopping at user request";
