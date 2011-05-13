747d749c64a85e41b25e728634c6fcd11d0e0545
Thad Ward <coderjoe69@yahoo.com>
2011-05-13 19:27:19 -0400

    fix broken thread cookie jar filename

--- bff-thread.sh
+++ bff-thread.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #./bff-thread.sh start end
 for ((id=${1}; id<=${2}; id++)); do
-  ./bff.sh $id cookies-\$\$.txt;
+  ./bff.sh $id cookies-$$.txt;
   if [ -f STOP ]; then
     echo "Stopping at user request";
     exit 0;
