8e28e7909f80df05f8c9461920ada6621f349c64
Thad Ward <coderjoe69@yahoo.com>
2011-05-15 00:11:01 -0400

    report profileid to caller via file

--- bff-thread.sh
+++ bff-thread.sh
@@ -6,10 +6,16 @@ if [ -z "$COOKIEJAR" ]; then
 	COOKIEJAR=cookies-$$.txt
 fi
 
+THREADSTATUS=bffthread-$$
+
 for ((id=${1}; id<=${2}; id++)); do
+  echo $id > $THREADSTATUS
   ./bff.sh $id $COOKIEJAR
   if [ -f STOP ]; then
     echo "Stopping at user request"
+    rm $THREADSTATUS
     exit 0
   fi
 done
+rm $THREADSTATUS
+
