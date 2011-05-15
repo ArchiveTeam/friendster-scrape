7bc0ffe5011cc2504ea04b1c48f6d110e9534881
Thad Ward <coderjoe69@yahoo.com>
2011-05-14 20:42:57 -0400

    add an optional cookie file parameter

--- bff-thread.sh
+++ bff-thread.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-#./bff-thread.sh start end
+#./bff-thread.sh start end [cookiefile]
+
+COOKIEJAR=$3
+if [ -z "$COOKIEJAR" ]; then
+	COOKIEJAR=cookies-$$.txt
+fi
+
 for ((id=${1}; id<=${2}; id++)); do
-  ./bff.sh $id cookies-$$.txt;
+  ./bff.sh $id $COOKIEJAR
   if [ -f STOP ]; then
-    echo "Stopping at user request";
-    exit 0;
-  fi;
+    echo "Stopping at user request"
+    exit 0
+  fi
 done
